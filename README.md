714db7836c2240c6ce9ffdd13d8d6169dde57b74
Davon Williams <davonwilliams@zapatatechnology.com>
2016-03-13 19:46:27 -0700

    added comments to file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HelloWorld
 # 3/13/2016 - Hello World Project created that will display "hello world" to the command line when run
+# 3/13/2016 - Added comments to the project to describe steps
--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//This method will print the hello world to the screen
 		System.out.println("Hello World!");
 	}
 
